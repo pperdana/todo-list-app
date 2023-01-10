5a9c3ac3e4c8b724630096a32d338e08b67403f7
Panji <panji9502@gmail.com>
2023-01-10 20:14:01 +0700

    add components and its functionallity

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import InputArea from "./components/input-area.component";
+import ToDoItem from "./components/todo-item.component";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [items, setItems] = useState([]);
+  function addItem(inputText) {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+  }
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea onAdd={addItem} />
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
